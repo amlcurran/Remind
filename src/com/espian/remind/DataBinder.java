06d1258e935268e9aa2d89732df12c64c8116c6e
Alex Curran <aml.curran@gmail.com>
2014-01-29 23:12:16 +0000

    Commit to appease the git gods and their confusion

--- src/com/espian/remind/DataBinder.java
+++
@@ -1,9 +0,0 @@
-package com.espian.remind;
-
-import android.view.View;
-
-public interface DataBinder<T> {
-    public View bindView(View recycledView, T data);
-
-    void unbindView(View view);
-}
